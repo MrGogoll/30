fd3e1be1fda2965d0ee183ff0b0139eec1b2f2d6
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-12-01 20:25:57 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,25 @@
+﻿using namespace std;
+#include <iostream>
+struct gnom {
+    int voz;
+    int ros;
+    char p;
+};
+
+int main()
+{
+    gnom a;
+    a.voz = 10;
+    a.ros = 134;
+    a.p = 'W';
+    gnom b;
+    b.voz = 38;
+    b.ros = 68;
+    b.p = 'M';
+    if (a.ros > b.ros) {
+        cout << a.voz;
+    }
+    if (a.ros < b.ros) {
+        cout << b.voz;
+    }
+}
